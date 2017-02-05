a81e017d266d914102179f772480d2168213133d
JayanthanRamalingam <nanban.alliswell1978@gmail.com>
2017-02-05 12:50:34 +0530

    basic programs in java
    Payilagam (Practical questions. - Chapter 1 to 5)

---
+++ classandmainmethod.java
@@ -0,0 +1,23 @@
+package jay;
+
+public class classandmainmethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte z=127;
+		int a=5;
+		float b=2.2f;
+		short c=230;
+		double d=450;
+		char e='s';
+		String f="multi char";
+				System.out.println(a);
+				System.out.println(b);
+				System.out.println(c);
+				System.out.println(d);
+				System.out.println(e);
+				System.out.println(f);
+				System.out.println(z);
+	}
+
+}
---
+++ welcome.java
@@ -0,0 +1,8 @@
+package jay;
+
+public class welcome {
+	public static void main(String args[])
+	{
+	System.out.println("Welcome to Payilagam");
+}
+}
